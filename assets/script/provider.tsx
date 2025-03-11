eeebbc7aa2da48d26f763c7057a9369ff39ac21b
Janyu <54945627+Janyu427@users.noreply.github.com>
2025-03-11 16:12:09 +0800

    remove unused

--- assets/script/provider.tsx
+++ assets/script/provider.tsx
@@ -7,18 +7,9 @@ import store from "@/redux/store";
 
 import Loading from "../../src/app/components/loading"
 
-// import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
-
 const App = ({ children }: any) => {
     return (
         <>
-            {/* <ProgressBar
-                    height="3px"
-                    color="#7c7c7c"
-                    options={{ showSpinner: false }}
-                    shallowRouting
-                /> */}
-
             <Provider store={store}>
                 <Loading />
                 {children}
