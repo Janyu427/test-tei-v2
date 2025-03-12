aaec42a6dbb9fd44ca73005912c9f363a9053b0b
Janyu <54945627+Janyu427@users.noreply.github.com>
2025-03-12 13:05:28 +0800

    fix breadcrumbs bug

--- src/app/components/breadcrumbs/desktop/index.tsx
+++ src/app/components/breadcrumbs/desktop/index.tsx
@@ -13,11 +13,14 @@ const App = async () => {
     const pathname = headersList.get("x-current-path")?.split("/")[1];
 
     const innerPageTitle = await api.innerPageTitle.getFetch(pathname);
-    const productDetails = await api.productDetails.getFetch(pathname);
 
     const pageName = innerPageTitle.title;
 
     if (pathname == "productDetails") {
+        const id = headersList.get("x-current-path")?.split("/")[2];
+        
+        const productDetails = await api.productDetails.getFetch(id);
+
         productName = productDetails.title;
     }
 
